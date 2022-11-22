03e198ad126c461819e9ff1a55ccab63cd8974e8
joesea <2969111832@qq.com>
2022-11-22 09:42:39 +0800

    STY: remove unnecessary codes and fix style

--- people/qiaohai.py
+++ people/qiaohai.py
@@ -1,5 +1,4 @@
-import numpy.matlib 
-import numpy as np 
+import numpy as np
 
 # Generate a diagonal matrix with 3 rows and 4 columns.  
-print (np.matlib.eye(n =  3, M =  4, k =  0, dtype =  float))
+print(np.eye(3, 4))
