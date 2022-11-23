1002a8c85c1d7157a30f147aad30c667a627c7cd
Zhouie <zhouie@tongji.edu.cn>
2022-11-23 18:59:55 +0800

    sty : change the style of indentation

--- people/zhoujie.py
+++ people/zhoujie.py
@@ -1,6 +1,10 @@
 import numpy as np 
  
 #Converts the list of tuples to ndarray
-list_tuples = [(1,2,3),(4,5)] 
+list_tuples = [
+    (1,2,3), 
+    (4,5,6),
+    (7,8,9)
+     ] 
 ndarry = np.asarray(list_tuples)  
 print(ndarry)
